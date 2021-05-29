82046e35cdb664e368bb6aac2141430c3acc248f
Junaid Shaikh <junaid.july@gmail.com>
2021-05-29 18:15:51 +0530

    added option to set cache-control header

--- webpack.s3.pusher.js
+++ webpack.s3.pusher.js
@@ -32,7 +32,7 @@ function S3PusherPlugin(options) {
 
   if (options.key && options.secret && (options.region || options.endpoint)) {
     const config = {
-      accessKeyId: options.key, 
+      accessKeyId: options.key,
       secretAccessKey: options.secret
     }
 
@@ -44,7 +44,7 @@ function S3PusherPlugin(options) {
       config.endpoint = options.endpoint
     }
 
-    AWS.config.update(config) 
+    AWS.config.update(config)
   }
 
   s3 = new AWS.S3();
@@ -56,13 +56,17 @@ function S3PusherPlugin(options) {
   if (options.acl) {
     this.acl = options.acl
   }
+
+  if (options.cache) {
+    this.cache = options.cache
+  }
 }
 
-S3PusherPlugin.prototype.removePaths = function(paths) {
+S3PusherPlugin.prototype.removePaths = function (paths) {
   var params = {
-    Bucket: this.bucket, 
+    Bucket: this.bucket,
     Delete: {
-      Objects: [], 
+      Objects: [],
       Quiet: false
     }
   };
@@ -73,30 +77,34 @@ S3PusherPlugin.prototype.removePaths = function(paths) {
     })
   }
 
-  s3.deleteObjects(params, function(err, data) {
-    if (err) console.log(err, err.stack); 
+  s3.deleteObjects(params, function (err, data) {
+    if (err) console.log(err, err.stack);
   });
 };
 
-S3PusherPlugin.prototype.removeSlashes = function(str) {
-    return str.replace(/^\/|\/$/g, '');
+S3PusherPlugin.prototype.removeSlashes = function (str) {
+  return str.replace(/^\/|\/$/g, '');
 };
 
-S3PusherPlugin.prototype.upload = function(filename, content) {
+S3PusherPlugin.prototype.upload = function (filename, content) {
   return new Promise((resolve, reject) => {
     let params = {
-      Bucket: this.bucket, 
-      Key: filename, 
+      Bucket: this.bucket,
+      Key: filename,
       Body: content
     }
 
     if (this.acl) {
-       params.ACL = this.acl
+      params.ACL = this.acl
+    }
+
+    if (this.cache) {
+      params.CacheControl = this.cache
     }
 
     let contentType = mime.lookup(filename)
     if (contentType) {
-        params.ContentType = contentType;
+      params.ContentType = contentType;
     }
 
     s3.putObject(params, (err, data) => {
@@ -109,18 +117,18 @@ S3PusherPlugin.prototype.upload = function(filename, content) {
   })
 };
 
-S3PusherPlugin.prototype.shouldUpload = function(filename) {
-  return (!this.include || this.include.test(filename)) && 
+S3PusherPlugin.prototype.shouldUpload = function (filename) {
+  return (!this.include || this.include.test(filename)) &&
     (!this.exclude || !this.exclude.test(filename));
 }
 
-S3PusherPlugin.prototype.log = function(message) {
+S3PusherPlugin.prototype.log = function (message) {
   if (!this.quiet) {
     console.log(message)
   }
 }
 
-S3PusherPlugin.prototype.apply = function(compiler) {
+S3PusherPlugin.prototype.apply = function (compiler) {
   compiler.hooks.emit.tapAsync("S3PusherPlugin", (compilation, callback) => {
     for (var filename in compilation.assets) {
       if (this.shouldUpload(filename)) {
@@ -136,37 +144,37 @@ S3PusherPlugin.prototype.apply = function(compiler) {
       this.log('\r\n\r\nUploading ' + this.assets.length + ' assets to \'' + this.bucket + '\'...')
 
       bar = new progress.SingleBar({
-          format: 'Progress | {bar} | {percentage}% | {filename}',
-          barCompleteChar: '\u2588',
-          barIncompleteChar: '\u2591',
-          hideCursor: true
+        format: 'Progress | {bar} | {percentage}% | {filename}',
+        barCompleteChar: '\u2588',
+        barIncompleteChar: '\u2591',
+        hideCursor: true
       });
-   
+
       bar.start(this.assets.length, 0, {
-          filename: "Starting..."
+        filename: "Starting..."
       });
 
       let c = 1;
       let error = false;
 
       for (var i in this.assets) {
-        // Remove the question mark. 
+        // Remove the question mark.
         // (added by laravel-mix for some assets)
         let filename = this.assets[i].split('?')[0];
 
         filename = this.removeSlashes(filename)
 
         let localPath = compiler.outputPath + '/' + filename;
-        let remotePath = filename;       
+        let remotePath = filename;
 
-        if (this.prefix) {        
+        if (this.prefix) {
           remotePath = this.prefix + '/' + remotePath;
         }
 
         await this.upload(remotePath, fs.readFileSync(localPath)).catch(e => {
           error = e
           cb(e)
-        }); 
+        });
 
         if (error) {
           return;
